19cf14b034998e8a2f637e69d7fd4a5273789876
Sebastian Albers <sebastian_albers@hotmail.com>
2017-05-09 19:09:59 +0200

    Setup basic

---
+++ src/main/java/com/albers/furnaceme/FurnaceMe.java
@@ -0,0 +1,28 @@
+package com.albers.furnaceme;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="furnaceme", name="FurnaceMe", version="1.11.2-0.1")
+public class FurnaceMe {
+
+    @Mod.Instance("furnaceme")
+    public static FurnaceMe instance;
+    
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
